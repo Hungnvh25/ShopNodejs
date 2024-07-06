f89cadfa60c11cc775c779b2c45dc1cbcbb7075a
Hungnvh25 <144328260+Hungnvh25@users.noreply.github.com>
2024-07-06 10:32:29 +0700

    start shop

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require("./src/app")
+
+const PORT = 3055
+
+
+
+const server = app.listen(PORT,()=>{
+    console.log(`WSV eCommerce start ${PORT}`);
+})
+
+process.on('SIGINT', ()=>{
+    server.close(()=> console.log(`Exit Server Express`))
+})
+
+
